2aeabcb7d92c318a88fe1ae79b6a0cedb6e855f5
songbing <songbing5132163.com>
2017-05-04 10:52:37 +0800

    match search方法

--- re_match.py
+++ re_match.py
@@ -5,7 +5,46 @@
 match函数试图从字符串的起始部分对模式进行匹配
 如果匹配成功，返回一个匹配对象；
 如果匹配失败，返回None
-
+match参数
+第一个参数是正则表达式，这里为"(\w+)\s"，如果匹配成功，则返回一个Match，否则返回一个None；
+第二个参数表示要匹配的字符串；
+第三个参数是标致位，用于控制正则表达式的匹配方式，如：是否区分大小写，多行匹配等等。
 """
 
+import re
 
+#通过正则进行匹配
+text = "JGood is a handsome boy, he is cool, clever, and so on..."
+m = re.match(r"(\w+)\s", text)
+print 'match'
+if m:
+    print m.group(0), '\n', m.group(1)
+else:
+    print "Not match"
+
+print "===================================="
+#匹配起始部分
+bt = 'bat bet bit'
+m = re.match('bat', bt)
+
+if m is not None:
+    print(m.group())
+else:
+    print("Not found bat")
+
+
+n = re.match('bit', bt)
+if n is not None:
+    print(n.group())
+else:
+    print("Not found bit")
+
+print "===================================="
+#通过正则扩展方法匹配字符串
+text = 'kitty kitty'
+w = re.match(r'\b(?P<word>\w+)\b\s+(?P=word)', text)#通过扩展指定需要匹配字符串的别名，可以后续调用
+
+print "字典",w.groupdict()#字典模式输出
+print w.group()
+print "指定序列号",w.group(1)
+print "指定名称",w.group('word')
--- re_search.py
+++ re_search.py
@@ -1,9 +1,63 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-
 """
 search函数试图从整个字符串搜索第一次出现匹配的情况
 如果匹配成功，返回一个对象;
 如果匹配失败，返回None
+"""
 
 """
+输出<a href="http://blog.csdn.net/watsy">watsy's blog</a> 中watsy's blog
+"""
+
+from bs4 import BeautifulSoup
+import re
+
+
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<a href="http://blog.csdn.net/watsy">watsy's blog</a>
+"""
+
+soup=BeautifulSoup(html,"html.parser")
+#print soup.prettify()
+# print soup.title
+# print soup.body
+#print soup.find_all("a")
+
+
+def blog_name():
+    for i in soup.find_all("a"):
+        i=str(i)
+        m=re.compile(r'<a.*class=.*')
+        if m.search(i) is None:
+            m1=re.search(r'.*">(.*)</a>.*',i,re.S)
+            return m1.group(1)
+
+print blog_name()
+
+print "=================================="
+
+text = "JGood is a handsome boy, he is cool, clever, and so on..."
+m1 = re.search(r'handsome\s', text)
+if m1:
+    print m1.group()
+else:
+    print "Not search"
+
+bt1 = ' bat bet bit'
+
+print "=================================="
+
+m1 = re.search('bat', bt1)
+if m1 is not None:
+    print(m1.group())
+else:
+    print('m1 not found')
